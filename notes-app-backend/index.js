cc4b9619cc3f5751cf64b59969facf07a1f0112a
esepgit <150302936+esepgit@users.noreply.github.com>
2024-02-08 15:54:42 -0300

    backend working

--- notes-app-backend/index.js
+++ notes-app-backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const logger = require('morgan')
 
 app.use(express.json())
+app.use(cors())
+app.use(logger('dev'))
 
 let notes = [
   {
